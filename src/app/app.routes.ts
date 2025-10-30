68516d02cce8a82b2d79c26ef755ffb7a65e3f00
jimee222 <jarayare@ucenfotec.ac.cr>
2025-10-30 06:27:54 -0600

    feat(frontend-routing): GuestGuard y rutas de register
    - solo permite /register a no autenticados
    - redirige a /app/dashboard si ya hay sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,17 @@ import { Register  } from './pages/register/register';
 import { Profile  } from './pages/profile/profile';
 import { About  } from './pages/about/about';
 import { NotFound  } from './pages/not-found/not-found';
+import { GuestGuard } from './guards/guest.guard';
+
 
 export const routes: Routes = [
   { path: '', component: Landing , pathMatch: 'full' },
   { path: 'about', component: About  },
   { path: 'login', component: Login  },
-  { path: 'register', component: Register  },
+  { path: 'register', 
+    component: Register, 
+    canActivate: [GuestGuard],
+   },
   { path: 'profile', component: Profile  },
   { path: '**', component: NotFound  },
 ];
---
+++ src/app/guards/guest.guard.ts
@@ -0,0 +1,16 @@
+// src/app/guards/guest.guard.ts
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const GuestGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const auth   = inject(AuthService);
+
+  // Permite entrar si NO hay sesión
+  if (!auth.check()) return true;
+
+  // Si ya hay sesión, redirige a donde prefieras
+  router.navigateByUrl('/app/dashboard');
+  return false;
+};
