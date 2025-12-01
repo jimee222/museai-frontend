92f2306b1bf9e9d88367b16c3be334a78ce5b0da
dcruzca <160550702+dcruzcastro@users.noreply.github.com>
2025-12-01 12:12:20 -0600

    Route Change in Menu
    fix: route changes in menu for quizzes based on role

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,6 +1,7 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,4 +10,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {}
+export class Menu {
+
+  isLogged = false;
+  isSuperAdmin = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.isLogged = this.auth.check();          
+    this.isSuperAdmin = this.auth.isSuperAdmin(); 
+  }
+
+  goToQuizMenu() {
+    if (!this.isLogged) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    if (this.isSuperAdmin) {
+      this.router.navigate(['/app/quizzes-admin']);
+    } else {
+      this.router.navigate(['/app/quizzes']);
+    }
+  }
+}
