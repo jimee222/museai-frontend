a6b0bd6c5077d29ca34f497e1fcaf868109cd90c
dcruzca <160550702+dcruzcastro@users.noreply.github.com>
2025-12-02 19:45:42 -0600

    Added Quizzes to Profile
    feat: added User's quizzes to profile tab

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, signal, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { Component, OnInit, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { IUser } from '../../interfaces';
 import { ProfileCardComponent } from '../../components/profile-card/profile-card.component';
+import { MyQuizzes } from '../../components/my-quizzes/my-quizzes.component';
+import { QuizService } from '../../services/quiz.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, ProfileCardComponent],
+  imports: [CommonModule, ProfileCardComponent, MyQuizzes],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrls: ['./profile.css'], 
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Profile implements OnInit {
@@ -18,11 +20,12 @@ export class Profile implements OnInit {
   user = signal<IUser | null>(null);
   loading = signal(true);
   error = signal<string | null>(null);
-  activeTab = signal<'info' | 'quizzes' | 'recorridos'>('info'); // 👈 nueva señal
+  activeTab = signal<'info' | 'quizzes'>('info');
 
   constructor(
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private quizService: QuizService
   ) {}
 
   ngOnInit(): void {
@@ -35,6 +38,12 @@ export class Profile implements OnInit {
       }
 
       this.user.set(currentUser);
+
+      
+      if (currentUser.id != null) {
+        this.quizService.loadAllAttempts(currentUser.id);
+      }
+
       this.loading.set(false);
 
     } catch (err) {
